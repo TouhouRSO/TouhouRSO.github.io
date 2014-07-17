86bc40325667f06792bab7ea66b6560accf0f394
Michael <wmichaelv@hotmail.com>
2014-07-17 14:04:43 -0700

    Update infoPhase.js

--- main/Layout/infoPhase.js
+++ main/Layout/infoPhase.js
@@ -1,21 +1,20 @@
 ;Quintus.Layout_InfoPhase = function(Q) {
 
-	Q.UI.Text.extend("infoPhase",{
-		init: function() {
+  Q.UI.Text.extend("infoPhase",{
+    init: function() {
       this._super({
         label: " Replenishing Phase ",
-		    color: "Black",
-		    align: 'center',
-	      x: 300,
-    		y: 540, // 1080 / 2
-    		border: false,
-      	setFont: "800 24px Arial"
+        color: "Black",
+        align: 'center',
+        x: 300,
+        y: 540, // 1080 / 2
+        border: false,
+        setFont: "800 24px Arial"
       });
       Q.state.on("change.phase",this,"phase");
     },
     phase: function(phase) {
-	      this.p.label = phase;
+      this.p.label = phase;
     }
-	});
-
+  });
 }
