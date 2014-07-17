cbbb22a7139a8bd2e8b6edabc7866db450b1a270
Michael <wmichaelv@hotmail.com>
2014-07-17 14:01:34 -0700

    Update supportParam.js

---
+++ main/Assets/card/supportParam.js
@@ -0,0 +1,13 @@
+;Quintus.Card_SupportParam = function(Q) {
+
+	Q.supportParam = {};
+
+	Q.supportParam["0609"]    = {character:"Alice1", sp:1, class:"doll", type:"leader"};
+	Q.supportParam["0610"]    = {character:"Alice1", sp:2, class:"doll", type:"leader"};
+	Q.supportParam["0611"]    = {character:"Alice2", sp:3, class:"doll", type:"leader"};
+	Q.supportParam["0612"]    = {character:"Alice2", sp:4, class:"none", type:"scene"};
+	Q.supportParam["0613"]    = {character:"Alice2", sp:2, class:"doll", type:"leader"};
+	Q.supportParam["0617"]    = {character:"Alice3", sp:1, class:"doll", type:"leader"};
+	Q.supportParam["0618"]    = {character:"Alice2", sp:2, class:"doll", type:"leader"};
+	Q.supportParam["0621"]    = {character:"Alice3", sp:4, class:"doll", type:"leader"};
+}
