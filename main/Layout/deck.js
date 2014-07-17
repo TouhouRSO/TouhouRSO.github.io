ec1123f449f5a4ce2217df77cca9599fec752c45
wmichaelv <wmichaelv@hotmail.com>
2014-07-17 04:32:49 -0700

    init deck

---
+++ main/Layout/deck.js
@@ -0,0 +1,28 @@
+;Quintus.Layout_Deck = function(Q) {
+	Q.UI.Container.extend("ownDeck",{
+		init: function() {
+      this._super({
+		  fill: "gray",
+      x: 1544, // 1620 - 24 - 52 (w/2)
+      y: 880, // 465 + 150 + 20 (border) + 150 + 20 (border) + 75 (h / 2)
+      w: 104,
+      h: 150,
+      border: false,
+      hidden: false
+      });
+    }
+	});
+  Q.UI.Container.extend("oppDeck",{
+    init: function() {
+      this._super({
+      fill: "gray",
+      x: 76,  // 24 + 52 (w / 2)
+      y: 200, // 465 - 20 (border) - 150 - 20 (border) - 75 (h / 2)
+      w: 104,
+      h: 150,
+      border: false,
+      hidden: false
+      });
+    }
+  });
+}
