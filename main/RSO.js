43d911bd546833bdcec288ba7a07c7cddba1c164
wmichaelv <wmichaelv@hotmail.com>
2014-07-16 21:28:53 -0700

    update main

--- main/RSO.js
+++ main/RSO.js
@@ -1,33 +1,39 @@
 window.addEventListener('load',function(e) {
 
-
+  /* Load All Libraries */
   var Q = window.Q = Quintus({
-    imagePath: "file:///C:/Rumbling%20Spell%20Orchestra/"
+    //imagePath: "file:///C:/Rumbling%20Spell%20Orchestra/"
   })
+  .include("Sprites, UI, Scenes, Input, Touch")
+  .include("Assets_ImageList, Assets_AudioList") 
+  .include("Card_CardBuilder, Card_Name")
+  .include("Layout_InfoPhase")
+  .include("Flow_PhaseManager");
 
+  /* Set Up The Canvas */
+  
+  /* Screen Size Ratio */
+  /* Desgined for 1920 * 1080 */
+  Q.setup({ 
+    width:   1920,
+    height:  1080,
+    scaleToFit: true,
+    upsampleWidth:  1024,  // Double the pixel density of the 
+    upsampleHeight: 768    // game if the w or h is 420x320
+  }).controls().touch();
+  
+  /* Load All Assets, then start the game */
+  //Q.preload(Q.imageList);
+  //Q.preload(Q.audioList);
 
-  .include("Sprites, UI, Scenes, Input, Touch");
-
-  Q.setup({width: 960, height: 900 })
-    .controls()
-    .touch();
-
-  Q.Sprite.extend("BG",{
-    init: function(p) {
-      this._super(p, { asset: "Alice/Alice0600.jpg", type: 0 });
-    }
-  });
-
-  /* Executed Commands */
-
-  Q.scene("startGame",function(stage) {  
-    /* Implement Objects */
-    var bg1 = stage.insert(new Q.BG({ name: "bg", x: 250, y: 300, z: 3,}));
-    console.log("lol");
-  });
-
-  Q.load("Alice/Alice0600.jpg", function() {
+  Q.preload("Hinafuda_-_Card_Back_small.jpg");
+  Q.preload(function() {
     Q.stageScene("startGame");
+  }, {
+    progressCallback: function(loaded,total) {
+      var element = document.getElementById("loading_progress");
+      element.style.width = Math.floor(loaded/total*100) + "%";
+    }
   });
 
 });
