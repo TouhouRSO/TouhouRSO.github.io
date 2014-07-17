6ac8cbbf8085908b1bbe9477271e50c7bf7c401f
Michael <wmichaelv@hotmail.com>
2014-07-17 14:01:24 -0700

    Update eventParam.js

---
+++ main/Assets/card/eventParam.js
@@ -0,0 +1,8 @@
+;Quintus.Card_EventParam = function(Q) {
+
+	Q.eventParam = {};
+
+	Q.eventParam["0614"]    = {character:"Alice2", sp:3, phase:" Replenishing Phase "};
+	Q.eventParam["0616"]    = {character:"Alice3", sp:3, phase:" Battle Phase "};
+	Q.eventParam["0619"]    = {character:"Alice1", sp:4, phase:" Replenishing Phase "};
+}
