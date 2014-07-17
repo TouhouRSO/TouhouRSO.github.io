01588160ad6e07fce9db8a142c119164643c6d35
Michael <wmichaelv@hotmail.com>
2014-07-17 14:01:13 -0700

    Update spellParam.js

---
+++ main/Assets/card/spellParam.js
@@ -0,0 +1,16 @@
+;Quintus.Card_SpellParam = function(Q) {
+
+	Q.spellParam = {};
+
+	Q.spellParam["0601"]    = {character:"Alice1", sp:1, type:"spread", av:1, iv:0, v:2};
+	Q.spellParam["0602"]    = {character:"Alice1", sp:2, type:"normal", av:1, iv:0, v:4};
+	Q.spellParam["0603"]    = {character:"Alice1", sp:2, type:"con",    av:1, iv:1, v:3};
+	Q.spellParam["0604"]    = {character:"Alice1", sp:3, type:"normal", av:2, iv:0, v:3};
+	Q.spellParam["0605"]    = {character:"Alice2", sp:2, type:"spread", av:1, iv:1, v:3};
+	Q.spellParam["0606"]    = {character:"Alice2", sp:4, type:"spread", av:2, iv:1, v:5};
+	Q.spellParam["0607"]    = {character:"Alice3", sp:5, type:"conc",   av:3, iv:0, v:5};
+	Q.spellParam["0608"]    = {character:"Alice3", sp:6, type:"normal", av:2, iv:1, v:6};
+	Q.spellParam["0615"]    = {character:"Alice3", sp:3, type:"con",    av:2, iv:0, v:5};
+	Q.spellParam["0620"]    = {character:"Alice1", sp:3, type:"spread", av:1, iv:1, v:4};
+
+}
